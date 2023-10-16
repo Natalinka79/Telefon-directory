e05fc11f135834a7d11520ba1e4f26329212fc5c
Nataliya <email@example.com>
2023-10-16 14:16:23 +0300

    3

--- logger.py
+++ logger.py
@@ -2,16 +2,16 @@ from data_create import input_user_data
 
 def input_data():
     name,surname, phone,address = input_user_data()
-    var = int(input(f'\nВ каком формате записать данные?\n'
-                    f'1 Вариант:\n'
-                    f'{name}\n'
-                    f'{surname}\n'
-                    f'{phone}\n'
-                    f'{address}\n\n'
-                    f'2 Вариант:\n'
-                    f'{name};{surname};{phone};{address}\n\n'
-                    f'\nВаш выбор: '
-                    ))
+    var = int(input( f'\nВ каком формате записать данные?\n'
+                     f'1 Вариант:\n'
+                     f'{name}\n'
+                     f'{surname}\n'
+                     f'{phone}\n'
+                     f'{address}\n\n'
+                     f'2 Вариант:\n'
+                     f'{name};{surname};{phone};{address}\n\n'
+                     f'\nВаш выбор: '
+                     ))
     if var == 1:
         with open("data_first_variant.csv", "r", encoding="utf-8") as file:   
             file.write(f'{name}\n'
